988f3d405b11597b1dd2ccfad27352246a969ce4
ariasatyaa <ariasatyaa@garena.co.id>
2024-07-14 00:02:12 +0000

    feat: improve queue page

--- src/pages/Queue/index.js
+++ src/pages/Queue/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { toast } from 'sonner';
 
 import { useAuth } from '../../components/layout';
@@ -11,13 +12,16 @@ function QueuePage() {
     try {
       await cancelQueue();
       await fetchQueue();
+      toast.success('Queue cancellation successful');
+      navigate('/category');
     } catch (error) {
       toast.error(error.message);
     }
   };
 
-  const handleClickRefreshButton = () => {
-    fetchQueue();
+  const handleClickRefreshButton = async () => {
+    await fetchQueue();
+    toast.success('Queue refreshed successfully');
   };
 
   const fetchQueue = async () => {
@@ -30,6 +34,7 @@ function QueuePage() {
     }
   };
 
+  const navigate = useNavigate();
   const [isFetched, setIsFetched] = useState(false);
   const [queueData, setQueueData] = useState(false);
   const { user } = useAuth();
@@ -58,35 +63,47 @@ function QueuePage() {
   return (
     <div className="flex min-h-full w-full flex-col pb-10 pt-7">
       <Breadcrumb label="Queue" />
-      <div className="text-gray-900 mb-2 mt-5 text-center text-2xl font-bold leading-9 tracking-tight">
-        Queue Num
-      </div>
-      <div className="text-gray-900 text-center text-4xl font-bold leading-9 tracking-tight">
-        {queueData.user_queue_number}
-      </div>
-      <div className="mt-10 flex flex-col items-center justify-center text-sm">
-        <div className="mb-2 text-center">
-          <h2 className="font-bold">Name</h2>
-          <p className="">{queueData.service_name}</p>
+      <div className="bg-white mt-6 rounded-lg pb-4 pt-2 shadow">
+        <div className="text-gray-900 text-center text-2xl font-bold">
+          Queue Num
         </div>
-        <div className="mb-2 text-center">
-          <h2 className="font-bold">Detail</h2>
-          <p className="">{queueData.service_details}</p>
-        </div>
-        <div className="mb-2 text-center">
-          <h2 className="font-bold">Address</h2>
-          <p className="">{queueData.service_address}</p>
-        </div>
-        <div className="mb-2 text-center">
-          <h2 className="font-bold">Phone</h2>
-          <p className="">{queueData.service_phone}</p>
-        </div>
-        <div className="mb-2 text-center">
-          <h2 className="font-bold">Price</h2>
-          <p className="">{queueData.service_price}</p>
+        <div className="text-gray-900 text-center text-6xl font-bold">
+          {queueData.user_queue_number}
         </div>
       </div>
-      <div className="mt-10 flex flex-row items-center justify-center">
+      <div className="bg-white mt-6 flex items-center justify-center rounded-lg py-4 shadow">
+        <div className="-ml-10 mr-10 flex h-60 w-60 items-center justify-center">
+          <img
+            className="h-full w-full object-cover"
+            src={queueData.service_image}
+            alt=""
+          />
+        </div>
+        <div className="flex flex-col text-sm">
+          <div className="mb-2">
+            <h2 className="font-bold">Name</h2>
+            <p className="">{queueData.service_name}</p>
+          </div>
+          <div className="mb-2">
+            <h2 className="font-bold">Detail</h2>
+            <p className="">{queueData.service_details}</p>
+          </div>
+          <div className="mb-2">
+            <h2 className="font-bold">Address</h2>
+            <p className="">{queueData.service_address}</p>
+          </div>
+          <div className="mb-2">
+            <h2 className="font-bold">Phone</h2>
+            <p className="">{queueData.service_phone}</p>
+          </div>
+          <div className="mb-2">
+            <h2 className="font-bold">Price</h2>
+            <p className="">{queueData.service_price}</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="mt-6 flex flex-row items-center justify-center">
         <button
           type="button"
           onClick={handleClickCancelButton}
